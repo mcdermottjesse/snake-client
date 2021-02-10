6fe684c970ec86427bc5be6edcfba28b37f13c49
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-02-10 20:42:54 +0000

    client side phase1

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  conn.setEncoding('utf8'); 
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+  conn.on('connect', () => {
+    conn.write('Name: ERM');
+  });
+  
+  return conn;
+};
+
+module.exports = {connect};
+
